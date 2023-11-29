868ed1c533c9fff62039269e3f63a514e1b00888
Pranav N <75573305+pranav-nb@users.noreply.github.com>
2023-11-29 13:50:59 +0530

    Update review2.cs

--- team6/Functions/review2.cs
+++ team6/Functions/review2.cs
@@ -1,1 +1,10 @@
-
+var name = "Ryan McDermott";
+ 
+public void PrintAsFirstNameAndLastName()
+{
+    var temp = name.Split(" ");
+    var firstName= temp[0];
+    var lastName= temp[1]
+    name = $"His first name is {firstName}, and his last name is {lastName}";
+    Console.WriteLine(name);
+}
