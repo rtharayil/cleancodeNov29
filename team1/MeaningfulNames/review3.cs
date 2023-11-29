4ea9475de6606babb235d8951d8bdc025ef482f8
Ranjith Tharayil <ranjiththarayil@gmail.com>
2023-11-29 11:52:45 +0530

    fix for merge issue

--- team1/MeaningfulNames/review3.cs
+++ team1/MeaningfulNames/review3.cs
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-// int iCounter;
-// string strFullName;
-// DateTime dModifiedDate;
 
-int counter;
-string fullName;
-DateTime modifiedDate;
-=======
 int Counter;
 string FullName;
 DateTime ModifiedDate;
->>>>>>> 208bc8570630a7295e875546de1f009b4cfbd1d9
+
