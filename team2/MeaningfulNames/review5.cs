e7d11bb3748e8739c567b96b5ded34cd91b3ea1b
prii2311 <147712668+prii2311@users.noreply.github.com>
2023-11-29 11:39:53 +0530

    Update review5.cs

--- team2/MeaningfulNames/review5.cs
+++ team2/MeaningfulNames/review5.cs
@@ -5,7 +5,7 @@ var songs = new List<string> { 'Back In Black', 'Stairway to Heaven', 'Hey Jude'
 var artists = new List<string> { 'ACDC', 'Led Zeppelin', 'The Beatles' };
 
 bool EraseDatabase() {}
-bool Restore_database() {}
+bool RestoreDatabase() {}
 
 class Animal {}
 class Alpaca {}
