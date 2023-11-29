cd4160307954740291f06120379535e58fdb67ab
sreenath707 <58086589+sreenath707@users.noreply.github.com>
2023-11-29 13:48:05 +0530

    Update review7.cs

--- team4/Functions/review7.cs
+++ team4/Functions/review7.cs
@@ -1,1 +1,23 @@
-
+class DBConnection
+{
+    private static DBConnection _instance;
+ 
+    private DBConnection()
+    {
+        // ...
+    }
+ 
+    public static DBConnection GetInstance() // no return type
+    {
+        if (_instance == null)
+        {
+            _instance = new DBConnection();
+        }
+ 
+        return _instance;
+    }
+ 
+    // ...
+}
+ 
+var singleton = DBConnection.GetInstance();
