bb2c1bc229823d404ebc18046c877f4c3e061906
Umang Agarwal <63835975+umang345@users.noreply.github.com>
2023-11-29 11:22:32 +0530

    Add files via upload

---
+++ team1/MeaningfulNames/review5.cs
@@ -0,0 +1,39 @@
+<<<<<<< HEAD
+// const int DAYS_IN_WEEK = 7;
+// const int daysInMonth = 30;
+
+// var songs = new List<string> { 'Back In Black', 'Stairway to Heaven', 'Hey Jude' };
+// var Artists = new List<string> { 'ACDC', 'Led Zeppelin', 'The Beatles' };
+
+// bool EraseDatabase() {}
+// bool Restore_database() {}
+
+// class animal {}
+// class Alpaca {}
+
+
+
+const int DAYS_IN_WEEK= 7;
+const int DAYS_IN_MONTH= 30;
+ 
+var songTitles = new List<string> { 'Back In Black', 'Stairway to Heaven', 'Hey Jude' };
+var artists = new List<string> { 'ACDC', 'Led Zeppelin', 'The Beatles' };
+ 
+bool ClearDatabase() {}
+bool RestoreDatabase() {}
+ 
+class Animal {}
+class Alpaca {}
+=======
+const int daysInWeek = 7;
+const int daysInMonth = 30;
+
+var songs = new List<string> { 'Back In Black', 'Stairway to Heaven', 'Hey Jude' };
+var artists = new List<string> { 'ACDC', 'Led Zeppelin', 'The Beatles' };
+
+bool EraseDatabase() {}
+bool Restore_database() {}
+
+class Animal {}
+class Alpaca {}
+>>>>>>> 208bc8570630a7295e875546de1f009b4cfbd1d9
