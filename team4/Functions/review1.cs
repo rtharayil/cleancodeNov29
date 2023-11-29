7ac0a93abac38115b7121104dd0584a757e4c654
sreenath707 <58086589+sreenath707@users.noreply.github.com>
2023-11-29 14:10:36 +0530

    Update review1.cs

--- team4/Functions/review1.cs
+++ team4/Functions/review1.cs
@@ -2,3 +2,35 @@
 // cryptographer.decrypt(user.getPhraseEncodedByPassword(), password); Directly we can use this no need of extra variable
 // "Valid Password".equals(cryptographer.decrypt(user.getPhraseEncodedByPassword(), password)
 // Not formatted
+
+package CS1;
+
+public class UserValidator {
+
+    private Cryptographer cryptographer;
+
+    public boolean checkPassword(String userName, String password) {
+        User user = UserGateway.findByName(userName);
+
+        if (user != null) {
+
+            String codedPhrase = user.getPhraseEncodedByPassword();
+
+            String phrase = cryptographer.decrypt(codedPhrase, password);
+
+            if ("Valid Password".equals(phrase)) {
+
+                session.initialize();
+
+                return true;
+            }
+
+
+        }
+        
+    
+        return false;
+    }
+
+
+}
