3b78a2617565a9066b0ebf2ca1ffc7342f5c2e7f
sreenath707 <58086589+sreenath707@users.noreply.github.com>
2023-11-29 13:16:35 +0530

    Update review3.cs

--- team4/Variables/review3.cs
+++ team4/Variables/review3.cs
@@ -1,8 +1,8 @@
 public long Fibonacci(int n)
 {
-    if (n < 50)
+    if (n < 50 && n>=0)
     {
-        if (n != 1 && n != 0)
+        if (n >1)
         {
             return Fibonacci(n - 1) + Fibonacci(n - 2);
         }
