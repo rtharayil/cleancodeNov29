eb1d46fb42307fbdee079c422e31315069188086
sreenath707 <58086589+sreenath707@users.noreply.github.com>
2023-11-29 11:54:18 +0530

    Create review3.cs

---
+++ team4/Variables/review3.cs
@@ -0,0 +1,18 @@
+public long Fibonacci(int n)
+{
+    if (n < 50)
+    {
+        if (n > 1)
+        {
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+        else
+        {
+            return n;
+        }
+    }
+    else
+    {
+        throw new System.Exception("Not supported");
+    }
+}
