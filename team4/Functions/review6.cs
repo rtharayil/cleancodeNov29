b478f2ac146978dc12cb98fe2791cc47d384d450
sreenath707 <58086589+sreenath707@users.noreply.github.com>
2023-11-29 14:09:30 +0530

    Update review6.cs

--- team4/Functions/review6.cs
+++ team4/Functions/review6.cs
@@ -1,1 +1,11 @@
-
+public void CreateFile(string fileName, bool temp = false)
+{
+    if (temp)
+    {
+        Touch("./temp/" + fileName);
+    }
+    else
+    {
+        Touch(fileName);
+    }
+}
