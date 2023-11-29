d63157c55eee99d67bda298ae91e5484c89e4153
sreenath707 <58086589+sreenath707@users.noreply.github.com>
2023-11-29 13:47:43 +0530

    Update review5.cs

--- team4/Functions/review5.cs
+++ team4/Functions/review5.cs
@@ -1,1 +1,14 @@
-
+public Path TravelToTexas(object vehicle)
+{
+    var texasLocation = new Location("texas");
+    if (vehicle.GetType() == typeof(Bicycle))
+    {
+        // (vehicle as Bicycle).PeddleTo(new Location("texas"));
+        vehicle.PeddleTo(texasLocation);
+    }
+    else if (vehicle.GetType() == typeof(Car))
+    {
+        // (vehicle as Car).DriveTo(new Location("texas"));
+        vehicle.DriveTo(texasLocation);
+    }
+}
