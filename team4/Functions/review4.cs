5939ab6207d231e94205e2eda8cae73b105e7ddb
sreenath707 <58086589+sreenath707@users.noreply.github.com>
2023-11-29 14:24:33 +0530

    Update review4.cs

--- team4/Functions/review4.cs
+++ team4/Functions/review4.cs
@@ -1,1 +1,62 @@
+[2:23 PM] Pandya, Nityanshu
+var map= new Dictionary<string,string>();
 
+map.add('777', 'Boing');
+
+map.add('Air Force One', 'AirForceOne');
+
+map.add('Cessna', 'Cessna');
+ 
+abstract class Airplane
+
+{
+
+    string _type;
+
+    // ...
+ 
+    abstract public double GetCruisingAltitude();
+ 
+}
+ 
+class Cessna : Airplane
+
+{
+
+    public double GetCruisingAltitude()
+
+    {
+
+        return GetMaxAltitude() - GetFuelExpenditure();
+
+    }
+
+}
+ 
+class AirForceOne : Airplane
+
+{
+
+    public double GetCruisingAltitude()
+
+    {
+
+        return GetMaxAltitude();
+
+    }
+
+}
+ 
+class Boing : Airplane
+
+{
+
+    public double GetCruisingAltitude()
+
+    {
+
+        return GetMaxAltitude() - GetPassengerCount();
+
+    }
+
+}
