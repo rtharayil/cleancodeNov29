c68e5a297c5199ca9e28defa41b85e2320af8daf
sreenath707 <58086589+sreenath707@users.noreply.github.com>
2023-11-29 13:57:12 +0530

    Update review10.cs

--- team4/Functions/review10.cs
+++ team4/Functions/review10.cs
@@ -1,1 +1,12 @@
-
+public class Email
+{
+    //...
+ 
+    public void Send()
+    {
+        SendMail(this._to, this._subject, this._body);
+    }
+}
+ 
+var message = new Email(...);
+message.Send();
