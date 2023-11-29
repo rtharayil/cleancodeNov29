7fcbf786c310eabf4b708c1dd715477fd0a9f6f7
sreenath707 <58086589+sreenath707@users.noreply.github.com>
2023-11-29 13:40:19 +0530

    Update review9.cs

--- team4/Functions/review9.cs
+++ team4/Functions/review9.cs
@@ -1,1 +1,11 @@
-
+public void SendEmailToClients(string[] clients)
+{
+    foreach (var client in clients)
+    {
+        var clientRecord = db.Find(client);
+        if (clientRecord.IsActive())
+        {
+            Email(client);
+        }
+    }
+}
