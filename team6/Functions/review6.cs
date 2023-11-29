f1e412cd759f863bff1fd115090acdd35a1cfb16
Pranav N <75573305+pranav-nb@users.noreply.github.com>
2023-11-29 14:01:23 +0530

    Update review6.cs

--- team6/Functions/review6.cs
+++ team6/Functions/review6.cs
@@ -1,1 +1,12 @@
-
+public void CreateFile(string name, bool temp)                                                                                                                                                                                                                             public void CreateFile(string name, bool temp)
+{
+    temp = false;
+    if (temp)
+    {
+        Touch("./temp/" + name);
+    }
+    else
+    {
+        Touch(name);
+    }
+}
