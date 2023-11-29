f5acc1f0783d465595a1457b32a3dabbd6bda669
Pranav N <75573305+pranav-nb@users.noreply.github.com>
2023-11-29 14:03:15 +0530

    Update review5.cs

--- team6/Functions/review5.cs
+++ team6/Functions/review5.cs
@@ -1,1 +1,11 @@
-
+public Path TravelToTexas(object vehicle)
+{
+    if (vehicle.GetType() == typeof(Bicycle))
+    {
+        (Bicycle).PeddleTo(new Location("texas"));
+    }
+    else if (vehicle.GetType() == typeof(Car))
+    {
+        (Car).DriveTo(new Location("texas"));
+    }
+}
