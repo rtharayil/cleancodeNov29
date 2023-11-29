0ef43e6f47432b9037ab9b662e2865044ea0fc77
Pranav N <75573305+pranav-nb@users.noreply.github.com>
2023-11-29 11:58:00 +0530

    Update review4.cs

---
+++ team6/Variables/review4.cs
@@ -0,0 +1,9 @@
+var cities = new[] { "Austin", "New York", "San Francisco" };
+ 
+for (var city = 0; i < cities.Count(); country++)
+{
+    var selectedCity = cities[i];
+    DoStuff();
+    DoSomeOtherStuff();
+    Dispatch(selectedCity);
+}
