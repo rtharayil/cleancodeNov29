7609bade8195197e17aaaafd49e7c6c4f2ca29ef
Pranav N <75573305+pranav-nb@users.noreply.github.com>
2023-11-29 14:03:46 +0530

    Update review4.cs

--- team6/Functions/review4.cs
+++ team6/Functions/review4.cs
@@ -1,1 +1,51 @@
+[1:58 PM] Loganath, Mamatha
+4.
+abstract class Airplane
 
+{
+
+    public abstract double GetCruisingAltitude();
+
+}
+ 
+class Boeing777 : Airplane
+
+{
+
+    public override double GetCruisingAltitude()
+
+    {
+
+        return GetMaxAltitude() - GetPassengerCount();
+
+    }
+
+}
+ 
+class AirForceOne : Airplane
+
+{
+
+    public override double GetCruisingAltitude()
+
+    {
+
+        return GetMaxAltitude();
+
+    }
+
+}
+ 
+class Cessna : Airplane
+
+{
+
+    public override double GetCruisingAltitude()
+
+    {
+
+        return GetMaxAltitude() - GetFuelExpenditure();
+
+    }
+
+}
