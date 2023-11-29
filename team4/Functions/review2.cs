a9bbb766e6ff6409ad3280453cec3e75b48ab6b2
sreenath707 <58086589+sreenath707@users.noreply.github.com>
2023-11-29 13:55:04 +0530

    Update review2.cs

--- team4/Functions/review2.cs
+++ team4/Functions/review2.cs
@@ -1,1 +1,16 @@
+var fullName = "Ryan McDermott";
+ 
+public void PrintNameDetails()
+{
+    var nameParts = fullName.Split(" ");
+    if (nameParts.Length == 2)
+    {
+        var formattedName = $"His first name is {nameParts[0]}, and his last name is {nameParts[1]}";
+        Console.WriteLine(formattedName);
+    }
+    else
+    {
+        Console.WriteLine("Invalid name format: Expected first and last name.");
+    }
+}
 
