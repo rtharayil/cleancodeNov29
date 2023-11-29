5ab225807f0f0ea889b59768dcef1ee6e7fb2d49
Pranav N <75573305+pranav-nb@users.noreply.github.com>
2023-11-29 14:09:22 +0530

    Create review13.cs

---
+++ team6/Functions/review13.cs
@@ -0,0 +1,8 @@
+public void NewRequestModule(string url)
+{
+    // ... Some logic
+}
+ 
+var request = NewRequestModule(requestUrl);
+// var request = OldRequestModule(requestUrl); Not in use 
+InventoryTracker("apples", request, "www.inventory-awesome.io");
