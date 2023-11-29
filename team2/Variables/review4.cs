10ea1cf4f9975684d92b34fb063cb6118b0e6ae1
prii2311 <147712668+prii2311@users.noreply.github.com>
2023-11-29 13:23:11 +0530

    Update review4.cs

--- team2/Variables/review4.cs
+++ team2/Variables/review4.cs
@@ -3,12 +3,7 @@ var city = new[] { "Austin", "New York", "San Francisco" };
 for (var index = 0; index < city.count(); index++)
 {
     var city_List = city[index];
-    DoStuff();
-    DoSomeOtherStuff();
-
-    // ...
-    // ...
-    // ...
-    // Wait, what is `li` for again?
+    DoInitialActivities();
+    DoSecondaryActivities();
     Dispatch(city_List);
 }
