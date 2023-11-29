8f8bda0a7573fa99a01bdfe7d577aeccc2ab8bf4
sreenath707 <58086589+sreenath707@users.noreply.github.com>
2023-11-29 13:54:34 +0530

    Update review1.cs

--- team4/Functions/review1.cs
+++ team4/Functions/review1.cs
@@ -1,1 +1,4 @@
-
+// with out creating object calling method cryptographer.decrypt(codedPhrase, password); leading to null pointer
+// cryptographer.decrypt(user.getPhraseEncodedByPassword(), password); Directly we can use this no need of extra variable
+// "Valid Password".equals(cryptographer.decrypt(user.getPhraseEncodedByPassword(), password)
+// Not formatted
